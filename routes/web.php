c7418adf97e7010a2e3060c6760912e911f7487f
vlvereta <vlvereta.92@gmail.com>
2019-06-30 12:19:51 +0300

    Fix routes.

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@
 |
 */
 
-Route::get('/{items?}', 'WebController@showMarket')->name('main');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('items/add', 'WebController@addProductForm')->name('add');
 
 Route::get('items/{id}', 'WebController@showProduct')->name('show');
 
+Route::get('/{items?}', 'WebController@showMarket')->name('main');
+
 Route::post('items', 'WebController@storeProduct')->name('store');
 
 Route::post('items/{id}', 'WebController@deleteProduct')->name('delete');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
